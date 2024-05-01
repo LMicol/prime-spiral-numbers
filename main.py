07f0c5df7afe7ac269a58475712650c0bae52597
LMicol <lucas.micol.p@gmail.com>
2024-05-01 12:30:00 -0300

    added is_prime and rearranged the code

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 
 import matplotlib.pyplot as plt
-from datetime import datetime
-import random
 import numpy as np
-import time
-import math
+import random
 import signal
+import time
 
 # Signal handler to exit gracefully
 def signal_handler(signal, frame):
@@ -17,31 +15,48 @@ def signal_handler(signal, frame):
 signal.signal(signal.SIGINT, signal_handler)
 signal.signal(signal.SIGTERM, signal_handler)
 
+def is_prime(n):
+    # this should work for up to several billions
+    if n <= 1:
+        return False
+    if n <= 3:
+        return True
+    if n % 2 == 0 or n % 3 == 0:
+        return False
+    i = 5
+    while i * i <= n:
+        if n % i == 0 or n % (i + 2) == 0:
+            return False
+        i += 6
+    return True   
 
-# Initialize plot
-plt.ion()
-fig, ax = plt.subplots(subplot_kw={'projection': 'polar'})
-r = []
-theta = []
+if __name__ == "__main__":
+    # Initialize plot
+    plt.ion()
+    fig, ax = plt.subplots(subplot_kw={'projection': 'polar'})
+    r = []
+    theta = []
 
-# Set up plot
-line, = ax.plot(theta, r)
-ax.set_rmax(2)
-ax.set_rticks([0.5, 1, 1.5, 2])
-ax.set_rlabel_position(-22.5)
-ax.grid(True)
-ax.set_title("A line plot on a polar axis", va='bottom')
+    # Set up plot
+    line, = ax.plot(theta, r)
+    ax.set_rmax(2)
+    ax.set_rticks([0.5, 1, 1.5, 2])
+    ax.set_rlabel_position(-22.5)
+    ax.grid(True)
+    ax.set_title("A line plot on a polar axis", va='bottom')
 
-# Update plot function
-def update_plot():
-    r.append(random.random() * 2)  # Random radial value between 0 and 2
-    theta.append(random.random() * 2 * np.pi)  # Random angle between 0 and 2*pi
-    line.set_xdata(theta)
-    line.set_ydata(r)
-    fig.canvas.draw()
-    fig.canvas.flush_events()
+    # Update plot function
+    def update_plot():
+        r.append(random.random() * 2)  # Random radial value between 0 and 2
+        theta.append(random.random() * 2 * np.pi)  # Random angle between 0 and 2*pi
+        line.set_xdata(theta)
+        line.set_ydata(r)
+        # Adjust radial range to zoom out
+        ax.set_rmax(max(r) + 1)  # Increase maximum radial value
+        fig.canvas.draw()
+        fig.canvas.flush_events()
 
-# Continuously update plot
-while True:
-    update_plot()
-    #time.sleep(0.1)
+    # Continuously update plot
+    while True:
+        update_plot()
+        time.sleep(0.1)
